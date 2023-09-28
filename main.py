00a4f78801a600b0f0d1b27e581da06b689d4fe2
gutodidonato <rm99433@fiap.com.br>
2023-09-28 18:37:45 -0300

    criada a lógica de movimentacao

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ corpo =[]
 game_is_on = True
 screen.tracer(0)
 
+def esquerda():
+    corpo[0].left(90)
+def direita():
+    corpo[0].right(90)
 
 for posicao in corpo_inicial:
     segmento = Turtle("square")
@@ -20,20 +24,20 @@ for posicao in corpo_inicial:
     segmento.goto(posicao)
     corpo.append(segmento)
     
-estado = "frente"
-
-def mover():
+while game_is_on:
+    screen.update()
+    time.sleep(1)
+    for segmento in range(len(corpo) -1 ,0, -1):
+        '''de trás para frente'''
+        posicao_x_nova = corpo[segmento - 1].xcor()
+        posicao_y_nova = corpo[segmento - 1].ycor()
+        corpo[segmento].goto(posicao_x_nova, posicao_y_nova)
+    corpo[0].forward(20)
+    direita()
+    '''
+    libera a restrição de ter que loopar cada um de uma vez, pq considera a movimentação dos segmentos anteriores
+    '''
     
-            
-            
-            
-                
-            
-            
-        
-mover()
- 
- 
 
 
 screen.exitonclick()
